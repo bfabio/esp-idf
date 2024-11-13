2e15c0ff58c86803ebe0a15eaa2be11476c81ad0
Fabio Bonelli <fb@fabiobonelli.it>
2024-11-13 11:18:46 +0100

    Make ad-hoc occupancy sensor

--- examples/zigbee/light_sample/HA_on_off_switch/main/switch_driver.c
+++ examples/zigbee/light_sample/HA_on_off_switch/main/switch_driver.c
@@ -41,44 +41,31 @@
 #include "freertos/queue.h"
 #include "switch_driver.h"
 
-/**
- * @brief:
- * This example code shows how to configure light switch with attribute as well as button switch handler.
- *
- * @note:
-   Currently only support toggle switch functionality available
- *
- * @note:
- * For other possible switch functions (on/off,level up/down,step up/down). User need to implement and create them by themselves
- */
+#define LD2410_OUTPUT_GPIO GPIO_NUM_12
 
 static QueueHandle_t gpio_evt_queue = NULL;
-/* button function pair, should be defined in switch example source file */
-static switch_func_pair_t *switch_func_pair;
 /* call back function pointer */
 static esp_switch_callback_t func_ptr;
-/* which button is pressed */
-static uint8_t switch_num;
-static const char *TAG = "ESP_ZB_SWITCH";
+
+static const char *TAG = "ESP_ZB_LD2410";
+static const IRAM_ATTR int ld2410_output_gpio = LD2410_OUTPUT_GPIO;
 
 static void IRAM_ATTR gpio_isr_handler(void *arg)
 {
-    xQueueSendFromISR(gpio_evt_queue, (switch_func_pair_t *)arg, NULL);
+    xQueueSendFromISR(gpio_evt_queue, (int*)arg, NULL);
 }
 
 /**
- * @brief Enable GPIO (switchs refer to) isr
+ * @brief Enable GPIO isr
  *
  * @param enabled      enable isr if true.
  */
 static void switch_driver_gpios_intr_enabled(bool enabled)
 {
-    for (int i = 0; i < switch_num; ++i) {
-        if (enabled) {
-            gpio_intr_enable((switch_func_pair + i)->pin);
-        } else {
-            gpio_intr_disable((switch_func_pair + i)->pin);
-        }
+    if (enabled) {
+        gpio_intr_enable(LD2410_OUTPUT_GPIO);
+    } else {
+        gpio_intr_disable(LD2410_OUTPUT_GPIO);
     }
 }
 
@@ -89,42 +76,33 @@ static void switch_driver_gpios_intr_enabled(bool enabled)
  */
 static void switch_driver_button_detected(void *arg)
 {
-    gpio_num_t io_num = GPIO_NUM_NC;
-    switch_func_pair_t button_func_pair;
-    static switch_state_t switch_state = SWITCH_IDLE;
-    bool evt_flag = false;
+    int pin;
+    bool first_run = true;
+
+    const TickType_t periodic_timeout_ticks = pdMS_TO_TICKS(5000);
 
     for (;;) {
-        /* check if there is any queue received, if yes read out the button_func_pair */
-        if (xQueueReceive(gpio_evt_queue, &button_func_pair, portMAX_DELAY)) {
-            io_num =  button_func_pair.pin;
+        if (first_run || xQueueReceive(gpio_evt_queue, &pin, periodic_timeout_ticks)) {
             switch_driver_gpios_intr_enabled(false);
-            evt_flag = true;
+
+            bool value = gpio_get_level(pin);
+            ESP_LOGD(TAG, "queue, pin: %d, level: %d", pin, value);
+
+            (*func_ptr)(&value);
+
+            switch_driver_gpios_intr_enabled(true);
+        } else {
+            switch_driver_gpios_intr_enabled(false);
+
+            bool value = gpio_get_level(LD2410_OUTPUT_GPIO);
+            ESP_LOGD(TAG, "pin: %d, level: %d", LD2410_OUTPUT_GPIO, value);
+
+            (*func_ptr)(&value);
+
+            switch_driver_gpios_intr_enabled(true);
         }
-        while (evt_flag) {
-            bool value = gpio_get_level(io_num);
-            switch (switch_state) {
-            case SWITCH_IDLE:
-                switch_state = (value == GPIO_INPUT_LEVEL_ON) ? SWITCH_PRESS_DETECTED : SWITCH_IDLE;
-                break;
-            case SWITCH_PRESS_DETECTED:
-                switch_state = (value == GPIO_INPUT_LEVEL_ON) ? SWITCH_PRESS_DETECTED : SWITCH_RELEASE_DETECTED;
-                break;
-            case SWITCH_RELEASE_DETECTED:
-                switch_state = SWITCH_IDLE;
-                /* callback to button_handler */
-                (*func_ptr)(&button_func_pair);
-                break;
-            default:
-                break;
-            }
-            if (switch_state == SWITCH_IDLE) {
-                switch_driver_gpios_intr_enabled(true);
-                evt_flag = false;
-                break;
-            }
-            vTaskDelay(10 / portTICK_PERIOD_MS);
-        }
+
+        first_run = false;
     }
 }
 
@@ -134,45 +112,40 @@ static void switch_driver_button_detected(void *arg)
  * @param button_func_pair      pointer of the button pair.
  * @param button_num            number of button pair.
  */
-static bool switch_driver_gpio_init(switch_func_pair_t *button_func_pair, uint8_t button_num)
+static bool switch_driver_gpio_init()
 {
-    gpio_config_t io_conf = {};
-    switch_func_pair = button_func_pair;
-    switch_num = button_num;
-    uint64_t pin_bit_mask = 0;
+    gpio_config_t io_conf = {
+        .pin_bit_mask = (1ULL << LD2410_OUTPUT_GPIO),
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_ANYEDGE
+    };
 
-    /* set up button func pair pin mask */
-    for (int i = 0; i < button_num; ++i) {
-        pin_bit_mask |= (1ULL << (button_func_pair + i)->pin);
-    }
-    /* interrupt of falling edge */
-    io_conf.intr_type = GPIO_INTR_NEGEDGE;
-    io_conf.pin_bit_mask = pin_bit_mask;
-    io_conf.mode = GPIO_MODE_INPUT;
-    io_conf.pull_up_en = 1;
-    /* configure GPIO with the given settings */
     gpio_config(&io_conf);
+
     /* create a queue to handle gpio event from isr */
-    gpio_evt_queue = xQueueCreate(10, sizeof(switch_func_pair_t));
+    gpio_evt_queue = xQueueCreate(10, sizeof(bool));
     if ( gpio_evt_queue == 0) {
         ESP_LOGE(TAG, "Queue was not created and must not be used");
+
         return false;
     }
-    /* start gpio task */
-    xTaskCreate(switch_driver_button_detected, "button_detected", 2048, NULL, 10, NULL);
+
     /* install gpio isr service */
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
-    for (int i = 0; i < button_num; ++i) {
-        gpio_isr_handler_add((button_func_pair + i)->pin, gpio_isr_handler, (void *) (button_func_pair + i));
-    }
+    gpio_isr_handler_add(LD2410_OUTPUT_GPIO, gpio_isr_handler, (void *) &ld2410_output_gpio);
+    /* start gpio task */
+    xTaskCreate(switch_driver_button_detected, "button_detected", 6144, NULL, 10, NULL);
+
+    switch_driver_gpios_intr_enabled(true);
+
     return true;
 }
 
-bool switch_driver_init(switch_func_pair_t *button_func_pair, uint8_t button_num, esp_switch_callback_t cb)
+bool switch_driver_init(esp_switch_callback_t cb)
 {
-    if (!switch_driver_gpio_init(button_func_pair, button_num)) {
-        return false;
-    }
     func_ptr = cb;
-    return true;
+
+    return switch_driver_gpio_init();
 }
